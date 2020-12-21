ee9c143761e499973d40139c0f0e51565fe3c863
Brian <neumaics@gmail.com>
2020-12-20 22:41:22 -0500

    site layout mostly worked out

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
- 
+import { createGlobalStyle } from 'styled-components';
+
 import { App } from './app';
  
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
+const GlobalStyle = createGlobalStyle`
+html {
+  height: 100%;
+  background-color: #0A0F12;
+  color: #fff;
+  font-family: 'IBM Plex Sans', sans-serif;
+}
+
+body {
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+
+#app {
+  height: 100%;
+}
+`;
+
+const app = (
+  <>
+    <GlobalStyle />
+    <App />
+  </>
 );
+
+ReactDOM.render(app, document.getElementById('app'));
