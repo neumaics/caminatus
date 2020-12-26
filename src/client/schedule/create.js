1f8d20a1adb0f742bdf630eb479a22dc76bd939f
Brian <neumaics@gmail.com>
2020-12-25 19:09:56 -0500

    updating form verbiage based on form fields
    this is all going to change anyway, once anyone who has used a kiln gets their hands on it...

--- src/client/schedule/create.js
+++ src/client/schedule/create.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { useForm, useFieldArray } from 'react-hook-form';
 
@@ -24,23 +24,8 @@ function Step() {
   return this;
 }
 
-function Rate() {
-  this.value = 0;
-  this.unit = '';
-  return this;
-}
-
-function Duration() {
-  this.value = 0;
-  this.unit = '';
-}
-
 const Form = styled.form`
   display: grid;
-`;
-
-const InputGroup = styled.label`
-
 `;
 
 export const CreateSchedule = () => {
@@ -48,7 +33,7 @@ export const CreateSchedule = () => {
   initialState.steps.push(new Step());
   initialState.steps.push(new Step());
 
-  const { register, control, handleSubmit } = useForm({ defaultValues: initialState });
+  const { register, control, handleSubmit, watch } = useForm({ defaultValues: initialState });
   const { fields, append, remove } = useFieldArray({ control, name: 'steps' });
 
   const saveSchedule = (data) => {
@@ -69,7 +54,7 @@ export const CreateSchedule = () => {
         <input
           type='text'
           name='name'
-          ref={register}
+          ref={register({ required: true, maxLength: 40, minLength: 2 })}
           autoComplete='off'
         />
       </label>
@@ -92,36 +77,45 @@ export const CreateSchedule = () => {
         })}
       </fieldset>
       
-      {(fields.map((step, i) => (
-        <div key={step.id}>
-          <button onClick={(e) => removeStep(e, i)}>-</button>
-          <select name={`steps[${i}].type`} ref={register()} defaultValue={step.type}>
-            <option value={STEP_TYPE.RATE}>rate</option>
-            <option value={STEP_TYPE.DURATION}>duration</option>
-          </select>
-          <input
-            name={`steps[${i}].startTemperature`}
-            defaultValue={step.startTemperature}
-            ref={register()}
-            type='number'
-          />
-          <input
-            name={`steps[${i}].endTemperature`}
-            defaultValue={step.endTemperature}
-            ref={register()}
-            type='number'
-          />
-          <input 
-            name={`steps[${i}].stepValue`}
-            type='number'
-            defaultValue={step.stepValue}
-            ref={register()}
-          />
-          <select name={`steps[${i}.unit]`} ref={register()} defaultValue={step.unit}>
-            {Object.values(TIME_SCALE).map(t => <option key={t} value={t}>{t}</option>)}
-          </select>
-        </div>
-      )))}
+      {fields.map((step, i) => {
+        const watchType = watch('steps', STEP_TYPE.DURATION);
+        return (
+          <div key={step.id}>
+            <button onClick={(e) => removeStep(e, i)}>-</button>
+            <input
+              name={`steps[${i}].startTemperature`}
+              defaultValue={step.startTemperature}
+              ref={register()}
+              type='number'
+              step='0.01'
+              min='0'
+            />
+            <input
+              name={`steps[${i}].endTemperature`}
+              defaultValue={step.endTemperature}
+              ref={register()}
+              type='number'
+              step='0.01'
+              min='0'
+            />
+            <select name={`steps[${i}].type`} ref={register()} defaultValue={step.type}>
+              <option value={STEP_TYPE.RATE}>by</option>
+              <option value={STEP_TYPE.DURATION}>over</option>
+            </select>
+            <input 
+              name={`steps[${i}].stepValue`}
+              type='number'
+              defaultValue={step.stepValue}
+              ref={register()}
+              min='0'
+              step='1'
+            />
+            {watchType[i].type === STEP_TYPE.RATE ? <span>per</span> : <span></span>}
+            <select name={`steps[${i}.unit]`} ref={register()} defaultValue={step.unit}>
+              {Object.values(TIME_SCALE).map(t => <option key={t} value={t}>{t}</option>)}
+            </select>
+          </div>); }
+      )}
       <button onClick={addStep}>+</button>
       <input type='submit'></input>
     </Form>
